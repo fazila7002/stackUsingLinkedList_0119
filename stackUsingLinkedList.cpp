59b1c30e533beec70463951a82beca898eb3beac
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-14 11:37:55 +0700

    Menambahkan fungsi peek untuk menampilkan seluruh isi stack dan menempatkan isEmpty

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -44,7 +44,22 @@ void pop()
     cout << "Popped value: " << top->data << endl;
 }
 
-
+void peek()
+{
+if (top == NULL)
+{
+cout << "List is empty." << endl;
+}
+else{
+Node *current = top;
+while (current != NULL)
+{
+cout << current->data << " " << endl;
+current = current->next;
+}
+cout << endl;
+}
+}
 
 bool isEmpty()
 {
