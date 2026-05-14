c36a70e99068edbdbd2209ed5b253fa40b428745
fazila7002 <fazilatunnisa705@gmail.com>
2026-05-14 11:50:25 +0700

    Case 2 untuk operasi Pop dengan validasi tumpukan kosong

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -92,6 +92,17 @@ int main()
         stack.push(value);
         break;
 
+        case 2:
+        if (!stack.isEmpty())
+        {
+                stack.pop();
+        }
+        else
+        {
+                cout << "Stack is empty. CAnnot pop." << endl;
+        }
+        break;
+
         
 
     }
